5ba8e54d4dce2faeba91a1ebaf6fd87e4a891e2f
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-01-27 17:14:27 -0500

    Precompilation blocks

--- lib/google-instant-hangouts/rails.rb
+++ lib/google-instant-hangouts/rails.rb
@@ -1,7 +1,10 @@
 module GoogleInstantHangouts
   module Rails
     class Engine < ::Rails::Engine
-    
+
+     initializer "precompile", :group => :all do |app|
+       app.config.assets.precompile << Proc.new{|path| path == "google-instant-hangouts.js" }
+    end
     end
   end
 end
