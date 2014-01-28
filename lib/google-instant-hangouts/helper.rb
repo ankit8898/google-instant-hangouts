566ee611037df0f2259ae4373586c092d3ff8f69
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-01-27 22:16:40 -0500

    Width and widget size support

--- lib/google-instant-hangouts/helper.rb
+++ lib/google-instant-hangouts/helper.rb
@@ -2,16 +2,17 @@ module GoogleInstantHangouts
 	module Helper
 		
 		def instant_hangout(opts={})
-			defaults = {render: 'createhangout',lang: 'en',room_id: 'my_room',topic: 'my topic',width: 200}
+			defaults = {render: 'createhangout',lang: 'en',room_id: 'my_room',topic: 'my topic',widget_size: 136,width: 300, hangout_type: nil}
 			defaults.merge!(opts)
-			content_tag(:div,
-				nil,
-				class: "instanthangouts",
-					render: defaults[:render],
-					lang: defaults[:lang],
-					room_id: defaults[:room_id],
-					topic: defaults[:topic],
-					width: defaults[:width])
+			content_tag(:div,nil,
+				         class: "instanthangouts",
+				         render: defaults[:render],
+				         lang: defaults[:lang],
+				         room_id: defaults[:room_id],
+				         topic: defaults[:topic],
+				         widget_size: defaults[:widget_size],
+				         width: defaults[:width],
+				         hangout_type: defaults[:hangout_type])
 		end
 	end
 end
